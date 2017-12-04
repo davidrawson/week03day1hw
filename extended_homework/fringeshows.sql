a5057d6b96d24155ac9d94a3c42eb2ef0775d097
David Rawson <dm.rawson@gmail.com>
2017-12-04 21:22:59 +0000

    Extensions started

--- extended_homework/fringeshows.sql
+++ extended_homework/fringeshows.sql
@@ -147,3 +147,41 @@ INSERT INTO "shows_users" (show_id, user_id) VALUES (13, 19);
 INSERT INTO "shows_users" (show_id, user_id) VALUES (12, 20);
 INSERT INTO "shows_users" (show_id, user_id) VALUES (11, 21);
 
+-- ## Section 1
+--
+--   This section involves more complex queries.  You will need to go and find out about aggregate funcions in SQL to answer some of the next questions.
+--
+--   1. Select the names and prices of all shows, ordered by price in ascending order.
+-- SELECT name, price FROM shows
+-- ORDER BY price ASC;
+--   10. Select the average price of all shows.
+--SELECT AVG(price) FROM shows;
+--   11. Select the price of the least expensive show.
+SELECT MIN(price) FROM shows;
+--   12. Select the sum of the price of all shows.
+SELECT SUM(price) FROM shows;
+--   13. Select the sum of the price of all shows whose prices is less than £20.
+SELECT SUM(price) FROM shows
+WHERE price < 20;
+--   14. Select the name and price of the most expensive show.
+SELECT MAX(price), name FROM shows;
+
+--   15. Select the name and price of the second from cheapest show.
+--
+--   16. Select the names of all users whose names start with the letter "M".
+--
+--   17. Select the names of users whose names contain "er".
+--
+--
+-- ## Section 2
+--
+--   The following questions can be answered by using nested SQL statements but ideally you should learn about JOIN clauses to answer them.
+--
+--   10. Select the time for the Edinburgh Royal Tattoo.
+--
+--   19. Select the number of users who want to see "Shitfaced Shakespeare".
+--
+--   20. Select all of the user names and the count of shows they're going to see.
+--
+--   21. SELECT all users who are going to a show at 17:15.
+--
